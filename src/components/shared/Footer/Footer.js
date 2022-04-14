4db61117e44f0c035084cb1e01b3d8e3d03578ca
Johann Pouponnot <johannpouponnot@MacBook-Pro-de-Johann.local>
2022-04-14 10:14:24 +0200

    feat: add Linkedin link in footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,14 +1,14 @@
 import "./footer.scss";
 import { MdiGithub } from "../../Logos/Github";
+import { BrandicoLinkedinRect } from "../../Logos/Linkedin";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="left">Johann Pouponnot @Le Reacteur - Andromeda 2022</div>
       <div className="right">
-        <a href="https://github.com/Joehann" target="_blank">
-          <MdiGithub />
-        </a>
+        <MdiGithub />
+        <BrandicoLinkedinRect />
       </div>
     </div>
   );
